688e522c4e3671728b469e34c1a3d404d2ac9d28
Matt Packwood <mattpackwood@gmail.com>
2019-01-12 11:26:18 -0500

    Code Upload

---
+++ WarmUp.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import java.applet.Applet;
+
+public class WarmUp extends Applet {
+public void paint(Graphics g) {
+		setBackground (Color.yellow); // change default BG color
+		g.drawString("Hello, I'm Matt and I just...", 18, 36);
+		g.drawString("LOVE Java!", 18, 54);
+	} //end paint
+}	//end Applet
